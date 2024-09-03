76e2e151f22b62d8e79b7ce09b3d0ec358cd93a4
Tanishqa Garg <tanishqagarg11@gmail.com>
2024-09-03 16:17:30 +0530

    Initial commit of Spring Boot project

---
+++ src/test/java/com/tanishqagarg/em_project/EmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanishqagarg.em_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
